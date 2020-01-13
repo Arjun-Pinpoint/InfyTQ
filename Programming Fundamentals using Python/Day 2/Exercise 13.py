7eeeec805be3aab01b101fd722045ff934e9cc8a
Arjun Pinpoint <59592004+Arjun-Pinpoint@users.noreply.github.com>
2020-01-13 17:30:11 +0530

    Create Exercise 13.py
    Write a JavaScript program to display the sum of two given numbers if the numbers are same. If not, display double the sum.
    Test your code by using the given sample input.
    Sample Input	Expected Output
    5,5	10

---
+++ Programming Fundamentals using Python/Day 2/Exercise 13.py
@@ -0,0 +1,12 @@
+'''
+Write a JavaScript program to display the sum of two given numbers if the numbers are same. If not, display double the sum.
+Test your code by using the given sample input.
+Sample Input	Expected Output
+5,5	10
+
+SOLUTION:
+'''
+//PF-Exer-13
+num1=5
+num2=5
+console.log(num1+num2)
